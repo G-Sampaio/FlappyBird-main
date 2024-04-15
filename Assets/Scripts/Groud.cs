95ee5cc70e1524b759332b9af88f3066632e043b
Guilherme Sampaio <guicwbforever@gmail.com>
2024-04-15 20:31:41 -0300

    V3.01

--- Assets/Scripts/Groud.cs
+++ Assets/Scripts/Groud.cs
@@ -2,38 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pipe : MonoBehaviour
+public class Groud : MonoBehaviour
 {
-    
+
     void Start()
     {
         
     }
-
     void Update()
     {
         switch (GameManager.instance.status)
         {
             case GameStatus.Start:
-                
                 break;
             case GameStatus.Play:
-                PlayUpdate();
+                playUpdate();
                 break;
             case GameStatus.GameOver:
                 break;
+            default:
+                break;
         }
-
     }
 
-    void PlayUpdate()
+    private void playUpdate()
     {
-        transform.position += Vector3.left * Time.deltaTime * GameManager.instance.speed;
-        if (transform.position.x < -2)
+        transform.position += Vector3.left * GameManager.instance.speed * Time.deltaTime;
+        if (transform.position.x < -0.04f)
         {
-            //autodestruição
-            Destroy(gameObject);
-
+            transform.position += Vector3.right * 0.24f;
         }
     }
 }
